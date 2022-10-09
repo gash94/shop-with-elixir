f3546eb9b8493478a52be881f3d9bcff14cde8bb
gash94 <104354207+gash94@users.noreply.github.com>
2022-10-09 14:54:44 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 // tablica z obiektami wszytskich mikstur (nazwa,cena)
-let potions = JSON.parse(window.localStorage.getItem('list-potions'));
+let potions = JSON.parse(window.localStorage.getItem("list-potions"));
 if (potions === null) {
     potions = [
         { name: "Speed potion", price: 460 },
@@ -10,21 +8,19 @@ if (potions === null) {
     ];
 }
 const atTheOldToad = {
-    
     getPotions() {
-        //Pokazuje wszytskie mikstury
-        const listShop = document.querySelector(".shop")
+        //Pokazuje wszystkie mikstury
+        const listShop = document.querySelector(".shop");
         listShop.innerHTML = "";
         const createItem = potions.map((potion) => {
             const listItem = document.createElement("li");
             listItem.classList.add("item");
             listItem.innerHTML = `Nazwa: "${potion.name}" Cena: ${potion.price}`;
-            
+
             return listItem;
-            
         });
         listShop.append(...createItem);
-        window.localStorage.setItem('list-potions', JSON.stringify(potions))   
+        window.localStorage.setItem("list-potions", JSON.stringify(potions));
     },
 
     addPotion(addNameInData, addPriceInData) {
@@ -32,14 +28,14 @@ const atTheOldToad = {
 
         for (const item of potions) {
             if (item.name === addNameInData) {
-                return Notify.warning(
+                return Notiflix.Notify.failure(
                     `Błąd! Eliksir ${addNameInData} jest już w Twoim inwentarzu!`
                 );
             }
         }
         potions.push({ name: addNameInData, price: addPriceInData });
         this.getPotions();
-        return alert(
+        return Notiflix.Notify.success(
             `Eliksir "${addNameInData}" z ceną: ${addPriceInData} został dodany do Twojego inwentarza!`
         );
     },
@@ -48,13 +44,15 @@ const atTheOldToad = {
         const potionIndex = potions.findIndex(
             (object) => object.name === potionName
         );
-       
+
         if (potionIndex === -1) {
-            return alert(`Eliksiru "${potionName}" nie ma w Twoim inwentarzu!`);
+            return Notiflix.Notify.failure(
+                `Eliksiru "${potionName}" nie ma w Twoim inwentarzu!`
+            );
         } else {
             potions.splice(potionIndex, 1);
             this.getPotions();
-            return alert(
+            return Notiflix.Notify.success(
                 `Eliksir "${potionName}" został usunięty z Twojego inwentarza!`
             );
         }
@@ -66,11 +64,15 @@ const atTheOldToad = {
         );
 
         if (potionIndex === -1) {
-            return alert(`Eliksiru "${oldName}" nie ma w Twoim inwentarzu!`);
+            return Notiflix.Notify.failure(
+                `Eliksiru "${oldName}" nie ma w Twoim inwentarzu!`
+            );
         }
         potions[potionIndex].name = newName;
         this.getPotions();
-        return alert(`Eliksir "${oldName}" zmienił nazwę na "${newName}"`);
+        return Notiflix.Notify.success(
+            `Eliksir "${oldName}" zmienił nazwę na "${newName}"`
+        );
     },
 };
 
@@ -81,15 +83,19 @@ const btnAdd = document.getElementById("addpotions");
 const btnRemove = document.getElementById("removepotions");
 const btnUpdate = document.getElementById("updatepotions");
 
-btnGet.addEventListener("click", atTheOldToad.getPotions);
+btnGet.addEventListener("click", () => {
+    
+    Notiflix.Notify.success("Pomyślnie odświeżono listę eliksirów");
+    atTheOldToad.getPotions();
+});
 
 btnAdd.addEventListener("click", () => {
     const addNameIn = document.getElementById("addname-input");
     const addPriceIn = document.getElementById("addprice-input");
-    
-    console.log(addPriceIn.value)
+
+    console.log(addPriceIn.value);
     if (addNameIn.value === "" || addPriceIn.value === "") {
-        return alert("Błąd! Uzupełnij wszytskie pola!");
+        return Notiflix.Notify.failure("Błąd! Uzupełnij wszystkie pola!");
     }
 
     atTheOldToad.addPotion(addNameIn.value, addPriceIn.value);
@@ -101,7 +107,7 @@ btnAdd.addEventListener("click", () => {
 btnRemove.addEventListener("click", () => {
     const addRemoveIn = document.getElementById("removename-input");
     if (addRemoveIn.value === "") {
-        return alert("Błąd! Uzupełnij pole!");
+        return Notiflix.Notify.failure("Błąd! Uzupełnij pole!");
     }
     atTheOldToad.removePotion(addRemoveIn.value);
     addRemoveIn.value = "";
@@ -111,7 +117,7 @@ btnUpdate.addEventListener("click", () => {
     const oldNameIn = document.getElementById("oldname-input");
     const newNameIn = document.getElementById("newname-input");
     if (oldNameIn.value === "" || newNameIn.value === "") {
-        return alert("Błąd! Uzupełnij wszytskie pola!");
+        return Notiflix.Notify.failure("Błąd! Uzupełnij wszystkie pola!");
     }
     atTheOldToad.updatePotionName(oldNameIn.value, newNameIn.value);
     oldNameIn.value = "";
